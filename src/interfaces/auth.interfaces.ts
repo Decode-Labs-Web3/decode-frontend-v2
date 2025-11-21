bbec8a9588e60a63fbb1a88e122c5c950fa3f02a
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-11-21 16:20:01 +0700

    update read me

--- src/interfaces/auth.interfaces.ts
+++ src/interfaces/auth.interfaces.ts
@@ -20,38 +20,43 @@ export interface RegisterData {
 }
 
 export interface Fingerprint {
-  "_id": string;
-  "user_id": string;
-  "device": string;
-  "browser": string;
-  "fingerprint_hashed": string;
-  "is_trusted": boolean;
-  "createdAt": string;
-  "updatedAt": string;
-  "__v": number;
-  "sessions": Session[];
+  _id: string;
+  user_id: string;
+  device: string;
+  browser: string;
+  fingerprint_hashed: string;
+  is_trusted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  sessions: Session[];
 }
 
 export interface Session {
-  "_id": string;
-  "user_id": string;
-  "device_fingerprint_id": string;
-  "session_token"?: string;
-  "app": string;
-  "expires_at": string;
-  "is_active": boolean;
-  "last_used_at": string;
-  "createdAt": string;
-  "updatedAt": string;
+  _id: string;
+  user_id: string;
+  device_fingerprint_id: string;
+  session_token?: string;
+  app: string;
+  expires_at: string;
+  is_active: boolean;
+  last_used_at: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface VerifyRequest {
   code: string;
-  type: 'register' | 'login' | 'forgot';
+  type: "register" | "login" | "forgot";
 }
 
 export interface UseVerificationProps {
-  type: 'register' | 'login' | 'forgot';
-  onSuccess: (data: { success: boolean; message: string; type: string; requiresRelogin?: boolean }) => void;
+  type: "register" | "login" | "forgot";
+  onSuccess: (data: {
+    success: boolean;
+    message: string;
+    type: string;
+    requiresRelogin?: boolean;
+  }) => void;
   onError?: (error: string) => void;
 }
